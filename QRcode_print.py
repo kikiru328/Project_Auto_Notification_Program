b4ddf5cb64dcc90fa6c4cbac159e0bb7adcfb6eb
Kwanghun Kim <60537388+kikiru328@users.noreply.github.com>
2021-08-16 12:34:53 +0900

    123

---
+++ QRcode_print.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+if __name_ == '__main__':
+    from PIL import Image
+    im = Image.open('D:\CAKD3\Workspace\- Lazy_project\AUTO_attendence_CHECK_Program\qrcode.png')
+    im.show()
+
+
+# In[ ]:
+
+
+
+
